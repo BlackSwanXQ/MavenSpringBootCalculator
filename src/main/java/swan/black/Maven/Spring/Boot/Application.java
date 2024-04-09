d0dd1ae3df8e947b8eb36055c053d9fe36f389de
Алексей Шлапаков <theblack2swan@gmail.com>
2024-04-09 18:12:57 +0300

    MavenSpringBootCalculator

---
+++ src/main/java/swan/black/Maven/Spring/Boot/Application.java
@@ -0,0 +1,13 @@
+package swan.black.Maven.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
